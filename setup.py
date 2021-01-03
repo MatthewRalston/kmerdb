b91c5b3905857cf370c2b1b30ce43129a89c1458
MatthewRalston <mrals89@gmail.com>
2021-01-03 11:01:50 -0500

    Removes redundant setup_yaml() function from stackoverflow, adds the function to kdb/util.py. Changes the LICENSE from GPLv3 to Apache.

--- setup.py
+++ setup.py
@@ -127,7 +127,7 @@ setup(
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)"
+        "License :: OSI Approved :: Apache Software License"
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
