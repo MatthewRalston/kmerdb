48b2379ab9b46bbbca64c2713f32d92c24a50c95
Matthew Ralston <mralston.development@gmail.com>
2021-01-08 22:47:36 -0500

    Fixes #16. Many changes but the most important to document was this, I'll explain in full in comments on the pull request from this issue.

--- kmerdb/probability.py
+++ kmerdb/probability.py
@@ -41,27 +41,29 @@ def markov_probability(seq:Bio.SeqRecord.SeqRecord, kdbrdr:fileutil.KDBReader, k
     :rtype:
     """
     if not isinstance(seq, Bio.SeqRecord.SeqRecord):
-        raise TypeError("kmerdb.probability.MarkovProbability expects a Bio.SeqRecord.SeqRecord as its first positional argument")
+        raise TypeError("kmerdb.probability.markov_probability expects a Bio.SeqRecord.SeqRecord as its first positional argument")
 
     elif not isinstance(kdbrdr, fileutil.KDBReader):
-        raise TypeError("kmerdb.probability.MarkovProbability expects a kmerdb.fileutil.KDBReader as its second positionl argument")
+        raise TypeError("kmerdb.probability.markov_probability expects a kmerdb.fileutil.KDBReader as its second positionl argument")
     elif not isinstance(kdbidx, index.IndexReader):
-        raise TypeError("kmerdb.probability.MarkovProbability expects a kmerdb.index.IndexReader as its third positional argument")
+        raise TypeError("kmerdb.probability.markov_probability expects a kmerdb.index.IndexReader as its third positional argument")
 
         
     k = kdbrdr.header['k']
+    mononucleotides = kdbrdr.header['files'][0]['mononucleotides']
     N = 4**k
     total_kmers = sum([f['total_kmers'] for f in kdbrdr.header['files']])
+    um = 1/N # This is the uniform frequency for the space
     if len(seq) < k:
-        raise ValueError("kmerdb.probability.MarkovProbability expects the sequence to be at least k={0} in length".format(k))
+        raise ValueError("kmerdb.probability.markov_probability expects the sequence to be at least k={0} in length".format(k))
     x = seq.seq[:k]
     kmer_id_x = kmer.kmer_to_id(x)
     if kmer_id_x > N:
-        raise ValueError("kmerdb.probability.MarkovProbability expected the k-mer id {0} of the subsequence x='{1}' to have an id less than N = 4^k = {2}".format(kmer_id_x, x, N))
+        raise ValueError("kmerdb.probability.markov_probability expected the k-mer id {0} of the subsequence x='{1}' to have an id less than N = 4^k = {2}".format(kmer_id_x, x, N))
     if not isinstance(kdbidx.index, np.ndarray):
-        raise ValueError("kmerdb.probability.MarkovProbability expects the kdbidx to be a Numpy array")
+        raise ValueError("kmerdb.probability.markov_probability expects the kdbidx to be a Numpy array")
     elif kdbidx.index.size != N:
-        raise ValueError("kmerdb.probability.MarkovProbability expects the kdbidx to have exactly {0} items, found {1}".format(N, kdbidx.size))
+        raise ValueError("kmerdb.probability.markov_probability expects the kdbidx to have exactly {0} items, found {1}".format(N, kdbidx.size))
 
     
     kmer_id, count, neighbors = index.read_line(kdbrdr, kdbidx, kmer_id_x)
@@ -75,36 +77,74 @@ def markov_probability(seq:Bio.SeqRecord.SeqRecord, kdbrdr:fileutil.KDBReader, k
     # Find the correct neighbor and calculate the transition probability
     prefix = x
     total = 0
+    total2 = 0
     product = 1
+    total_nucleotides = sum(mononucleotides.values())
+    logger.debug(seq.seq)
+    logger.debug(len(seq.seq))
     for i in range(k, len(seq.seq)):
+
         sum_qses = 0
-        for s in neighbors["suffixes"]:
-            kmer_id, count, _ = index.read_line(kdbrdr, kdbidx, s)
+        sum_aij = 0
+
+        for char, idx in neighbors["suffixes"].items():
+            kmer_id, count, _ = index.read_line(kdbrdr, kdbidx, idx)
             if kmer_id is None or count is None or _ is None:
-                raise ValueError("k-mer id '{0}' had an offset of zero in the index, it was not observed in the genome. Effective probability of sequence is 0 and Log-odds ratio of the sequence being generated from the k-mer profile is effectively 0 as well.")
-            if s != kmer_id:
-                raise ValueError("k-mer id read from file did not match the provided neighbor suffix id")
-            sum_qses += count/float(total_kmers)
+                kmer_id = idx
+                count = 0
+                #raise ValueError("k-mer id '{0}' had an offset of zero in the index, it was not observed in the genome. Effective probability of sequence is 0 and Log-odds ratio of the sequence being generated from the k-mer profile is effectively 0 as well.".format(s))
+            aij = mononucleotides / total_nucleotides
+            sum_aij += log10(mononucleotides[char] / total_nucleotides)
+            sum_qses += log10(count/float(total_kmers))
         # Prefix is the next prefix sequence to find a suffix for
-        prefix = prefix[1:] + seq.seq[i]
-        # We take the prefixes count and new neighbors
-        kmer_id, count, neighbors = index.read_line(kdbrdr, kdbidx, kmer.kmer_to_id(prefix))
-        # Now we calculate qt similar to px, the frequency (count / total number of k-mers, read from the header)
-        qt = count/float(total_kmers)
-        # Now we calculate the transition probability of sequence s to sequence t
-        # as qt divided by the sum of each qsc
-        ast = qt / sum_qses
-        # For the final log odds ratio, we sum the logs of the transition probabilities
-        total += math.log(ast)
-        # For the final probability, we multiply the transition probabilities
-        product *= ast
+        if i >= len(seq.seq):
+            break
+            #raise ValueError("kmerdb.probability.markov_probability encountered a value of i={0} larger than the length of the sequence {1}".format(i, len(seq.seq)))
+        elif seq.seq[i] == "":
+            logger.warning("Reached the end of the sequence, i={0} is producing no letters".format(i))
+            break
+        else:
+            logger.debug("{0} : prefix: '{1}' + '{2}'".format(i, prefix[1:], seq.seq[i]))
+            prefix = prefix[1:] + seq.seq[i]
+
+        if len(prefix) != k:
+            break
+        else:
+            logger.debug("New k-mer: '{0}'".format(prefix))
+            logger.debug("Length of prefix: {0}".format(len(prefix)))
+            try:
+                kmerid = kmer.kmer_to_id(prefix)
+            except KeyError as e:
+                logger.debug(i)
+                logger.debug("this should be the new letter: '{0}'".format(seq.seq[i]))
+                logger.debug("this should be the existing prefix: '{0}'".format(prefix))
+                raise e
+            logger.debug("Iteration number {0}, calculating probabilities for k-mer id={1} seq={2}".format(i, kmerid, prefix))
+            logger.debug("Reading counts/neighbors from file according to k-mer id")
+            # We take the prefixes count and new neighbors
+            kmer_id, count, neighbors = index.read_line(kdbrdr, kdbidx, kmerid)
+
+            # Now we calculate qt similar to px, the frequency (count / total number of k-mers, read from the header)
+        
+            qt = count/float(total_kmers)
+            # Now we calculate the transition probability of sequence s to sequence t
+            # as qt divided by the sum of each qsc
+            ast = qt / sum_qses
+
+            # For the final log odds ratio, we sum the logs of the transition probabilities
+            total += math.log10(ast)
+            total2 += math.log10(sum_aij) # The ratio of the uniform frequency to the first order Markov background
+            # For the final probability, we multiply the transition probabilities
+            product *= ast
+            product2 *= aij
     # Fianlly qxis represents our null model in a way.
     # Suppose that the null model consisted of the same number of k-mer as observed,
     # but uniformly distributed amongst all the k-mers
-    qxis = [total_kmers/N for x in range(k, len(seq.seq))]
+    qxis = [um for x in range(k, len(seq.seq))]
     # We put this on the bottom of the Log-odds Ratio because the null model contrasts the transition probabilities
-    lor = log(px) + total / sum([math.log(qx) for qx in qxis])
+    null_model = math.log10(um) + total2
+    lor = (math.log10(px) + total) / null_model
     # 
     pseq = px*product#/functools.reduce(lambda x,y: x*y, qxis)
-
+    prand = product2/N
     return {"seq": seq, "log_odds_ratio": lor, "p_of_seq": pseq}
