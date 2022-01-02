5d150ec88d8aee91d167aedcb1c22f12c4996d6a
Matthew Ralston <mralston.development@gmail.com>
2022-01-02 02:55:43 -0500

    Removes postgresql database layer, dramatically reducing runtime of profile generation.

--- .clean_local_install.sh
+++ .clean_local_install.sh
@@ -2,4 +2,4 @@
 
 
 
-rm -rf /home/matt/.pyenv/versions/kdb/lib/python3.8/site-packages/kmerdb /home/matt/.pyenv/versions/kdb/lib/python3.8/site-packages/kmerdb-*.egg-info /ffast/kdb/kmerdb.egg-info
+rm -rf /home/matt/.pyenv/versions/kdb/lib/python3.10/site-packages/kmerdb /home/matt/.pyenv/versions/kdb/lib/python3.10/site-packages/kmerdb-*.egg-info /ffast2/kdb/kmerdb.egg-info
