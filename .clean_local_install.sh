f920926e39b0ce900b31c47145877f70815bef02
MatthewRalston <mrals89@gmail.com>
2022-01-18 12:31:08 -0500

    Tidying the tidyer.

--- .clean_local_install.sh
+++ .clean_local_install.sh
@@ -2,4 +2,4 @@
 
 
 
-rm -rf /home/matt/.pyenv/versions/kdb/lib/python3.10/site-packages/kmerdb /home/matt/.pyenv/versions/kdb/lib/python3.10/site-packages/kmerdb-*.egg-info /ffast2/kdb/kmerdb.egg-info
+rm -rf /home/matt/.pyenv/versions/kdb/lib/python3.10/site-packages/kmerdb /home/matt/.pyenv/versions/kdb/lib/python3.10/site-packages/kmerdb-*.egg-info /ffast2/kdb/kmerdb.egg-info /ffast2/kdb/build /ffast2/kdb/dist
