b0ee537ef0d81a82537d5b8759869eaa48bfc981
Matthew Ralston <mralston.development@gmail.com>
2022-01-18 04:50:31 -0500

    Fixes #57. Too much hotfixing, can't get everything tonight. Will come back at it.

--- kmerdb/distance.py
+++ kmerdb/distance.py
@@ -23,6 +23,7 @@ logger = logging.getLogger(__file__)
 
 import math
 import numpy as np
+import sys
 #from numba import jit
 import functools
 
@@ -33,16 +34,66 @@ identity = {
     'correlation': '1.0',
     'euclidean'  : '0.0',
     'hamming'    : '1.0',
+    'pearson'    : '1.0',
     'spearman'   : '1.0'
 }
 
+def correlation(prof1:fileutil.KDBReader, prof2:fileutil.KDBReader, k=None, dtype="uint32"):
+    
+    if type(prof1) is not fileutil.KDBReader:
+        raise TypeError("kmerdb.distance.correlation expects a KDBReader as its first positional argument")
+    elif type(prof2) is not fileutil.KDBReader:
+        raise TypeError("kmerdb.distance.correlation expects a KDBReader as its second positional argument")
+    elif prof1.dtype != dtype:
+        raise TypeError("kmerdb.distance.correlation expects a KDBReader of '{0}' as its first argument".format(dtype))
+    elif prof2.dtype != dtype:
+        raise TypeError("kmerdb.distance.correlation expects a KDBReader of '{0}' as its second argument".format(dtype))
+    elif k is None:
+        raise ValueError("Cannot calculate correlation without k")
+    p1_size = int(prof1.profile.size)
+    p2_size = int(prof2.profile.size)
+    if p1_size == p2_size:
+        #print(p1_size)
+        #print(p2_size)
+        #print("Trying to calculate distance. Sizes are equal.")
+        #sys.exit(1)
+        pass
+    if k is not None and p1_size != p2_size:
+        logger.error(p1_size)
+        logger.error(p2_size)
+        sys.exit(1)
+        raise ValueError("Cannot calculate correlation of unequal size NumPy arrays.")
 
-def correlation(fname1, fname2):
+    N = 4**k
+    if p1_size == p2_size:
+        x_bar = np.sum(prof1.profile)/prof1.metadata["total_kmers"]
+        y_bar = np.sum(prof2.profile)/prof2.metadata["total_kmers"]
+
+        ssxx = 0
+        ssyy = 0
+        ssxy = 0
+        for kmer_id in range(N):
+            x = prof1.profile[kmer_id]
+            y = prof2.profile[kmer_id]
+            ssxx += np.square(x - x_bar)
+            ssyy += np.square(y - y_bar)
+            x_minus_xbar = x - x_bar
+            y_minus_ybar = y - y_bar
+            ssxy += x_minus_xbar*y_minus_ybar
+            #logger.warning("Beware custom correlations")
+        #x_bar = functools.reduce(lambda a,b: a+b, prof1))
+    else:
+        logger.error("Cannot manage to compare a {0}-dim profile with a {1}-dim profile...".format(p1_size, p2_size))
+        raise ValueError("Cannot calculate the correlation coefficient of unequal arrays.")
+
+    
+def _correlation(fname1, fname2):
     if type(fname1) is not str:
         raise TypeError("kmerdb.distance.correlation expects a str as its first positional argument")
     elif type(fname2) is not str:
         raise TypeError("kmerdb.distance.correlation expects a str as its second positional argument")
     k = None
+    logger.info("Custom correlation coefficient calculation")
     with fileutil.open(fname1, mode='r') as kdb1:
         with fileutil.open(fname2, mode='r') as kdb2:
             if k is None:
@@ -66,7 +117,10 @@ def correlation(fname1, fname2):
                 ssxx += np.square(x - x_bar)
                 ssyy += np.square(y - y_bar)
                 ssxy += (x - x_bar)*(y - y_bar)
-
+                #logger.debug("int through np.type on custom correlation function")
+                #logger.debug("Style points")
+                logger.info("Squared differences.")
+                logger.info("Symmetry")
             logger.debug("Sum of squared xy errors: {0}".format(ssxy))
             logger.debug("Sum of squared xx errors: {0}".format(ssxx))
             logger.debug("Sum of squared yy errors: {0}".format(ssyy))
@@ -84,6 +138,7 @@ def euclidean(fname1, fname2):
     elif type(fname2) is not str:
         raise TypeError("kmerdb.distance.euclidean expects a str as its second positional argument")
     k = None
+    logger.debug("Custom euclidean distance starting...")
     sum_of_squared_differences = 0
     with fileutil.open(fname1, mode='r') as kdb1:
         with fileutil.open(fname2, mode='r') as kdb2:
@@ -101,6 +156,9 @@ def euclidean(fname1, fname2):
                 _, x = (int(_x) for _x in line1.rstrip().split("\t"))
                 _, y = (int(_y) for _y in line2.rstrip().split("\t"))
                 sum_of_squared_differences += (x - y)**2
+                logger.debug("Sqrt | squared differences ((x-y)^2) ")
+                logger.info("Calculating custom euclidean distance function")
+                logger.debug("Self care...")
             return sqrt(sum_of_squared_differences)
 
 
@@ -111,6 +169,8 @@ def spearman(x, y):
         raise TypeError("kmerdb.distance.spearman expects a Numpy array as its second positional argument")
     from scipy.stats import spearmanr
     cor, pval = spearmanr(x, b=y)
+    logger.debug("Spearman calculation from SciPy")
+    logger.info("Smooth, buttery Spearman correlation coefficients.")
     return cor, pval
 
 def EMD(x, y):
