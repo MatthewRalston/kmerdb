39d4c31d64e1e7bbd10628e4b31b418d31368fc5
Matthew Ralston <mralston.development@gmail.com>
2022-01-23 21:51:41 -0500

    Reworked pearson to do custom correlation coefficient. Removed correlation from secondary distance menu. Now in primary scipy space.

--- kmerdb/distance.py
+++ kmerdb/distance.py
@@ -38,103 +38,142 @@ identity = {
     'spearman'   : '1.0'
 }
 
-def correlation(prof1:fileutil.KDBReader, prof2:fileutil.KDBReader, k=None, dtype="uint32"):
-    
-    if type(prof1) is not fileutil.KDBReader:
-        raise TypeError("kmerdb.distance.correlation expects a KDBReader as its first positional argument")
-    elif type(prof2) is not fileutil.KDBReader:
-        raise TypeError("kmerdb.distance.correlation expects a KDBReader as its second positional argument")
-    elif prof1.dtype != dtype:
-        raise TypeError("kmerdb.distance.correlation expects a KDBReader of '{0}' as its first argument".format(dtype))
-    elif prof2.dtype != dtype:
-        raise TypeError("kmerdb.distance.correlation expects a KDBReader of '{0}' as its second argument".format(dtype))
-    elif k is None:
-        raise ValueError("Cannot calculate correlation without k")
-    p1_size = int(prof1.profile.size)
-    p2_size = int(prof2.profile.size)
-    if p1_size == p2_size:
-        #print(p1_size)
-        #print(p2_size)
-        #print("Trying to calculate distance. Sizes are equal.")
-        #sys.exit(1)
-        pass
-    if k is not None and p1_size != p2_size:
-        logger.error(p1_size)
-        logger.error(p2_size)
-        sys.exit(1)
-        raise ValueError("Cannot calculate correlation of unequal size NumPy arrays.")
 
-    N = 4**k
-    if p1_size == p2_size:
-        x_bar = np.sum(prof1.profile)/prof1.metadata["total_kmers"]
-        y_bar = np.sum(prof2.profile)/prof2.metadata["total_kmers"]
+# Remove fileutil.KDBReader references
+# Add numpy structure
+#
+#
+#
+#
+#
+def correlation(counts1:np.array, counts2:np.array, dtype="uint64"):
 
-        ssxx = 0
-        ssyy = 0
-        ssxy = 0
-        for kmer_id in range(N):
-            x = prof1.profile[kmer_id]
-            y = prof2.profile[kmer_id]
-            ssxx += np.square(x - x_bar)
-            ssyy += np.square(y - y_bar)
-            x_minus_xbar = x - x_bar
-            y_minus_ybar = y - y_bar
-            ssxy += x_minus_xbar*y_minus_ybar
-            logger.warning("Beware custom correlations")
-        #x_bar = functools.reduce(lambda a,b: a+b, prof1))
-        if ssxx*ssyy == 0:
-            logger.error("Incorrect denominator found, skipping...")
-            return 0
-        else:
-            logger.info("Acquired")
-            logger.debug("Well done, cap...")
-            return ssxy/np.sqrt(ssxx*ssyy)
+    if type(counts1) is not np.array:
+        raise TypeError("kmerdb.distance.correlation expects a NumPy array as its first positional argument")
+    elif type(counts2) is not np.array:
+        raise TypeError("kmerdb.distance.correlation expects a NumPy array as its second positional argument")
+    elif counts1.size != counts2.size:
+        raise TypeError("kmerdb.distance.correlation expects two equal size NumPy arrays as its first and second positional arguments.")
+    x_bar = np.sum(counts1)/counts1.size
+    y_bar = np.sum(counts2)/counts2.size
+
+    ssxx = 0
+    ssyy = 0
+    ssxy = 0
+    for i, c in enumerate(counts1):
+        x = c
+        y = counts2[i]
+        ssxx += np.square(x - x_bar)
+        ssyy += np.square(y - y_bar)
+        x_minus_xbar = x - x_bar
+        y_minus_ybar = y - y_bar
+        ssxy += x_minus_xbar*y_minus_ybar
+        logger.warning("Beware custom correlations")
+    if ssxx*ssyy == 0:
+        logger.error("Incorrect denominator found, skipping...")
+        return 0
     else:
-        logger.error("Cannot manage to compare a {0}-dim profile with a {1}-dim profile...".format(p1_size, p2_size))
-        raise ValueError("Cannot calculate the correlation coefficient of unequal arrays.")
+        logger.info("Acquired")
+        #logger.debug("Well done, cap...")
+        return ssxy/np.sqrt(ssxx*ssyy)
+
+# def correlation(prof1:fileutil.KDBReader, prof2:fileutil.KDBReader, k=None, dtype="uint32"):
+    
+#     if type(prof1) is not fileutil.KDBReader:
+#         raise TypeError("kmerdb.distance.correlation expects a KDBReader as its first positional argument")
+#     elif type(prof2) is not fileutil.KDBReader:
+#         raise TypeError("kmerdb.distance.correlation expects a KDBReader as its second positional argument")
+#     elif prof1.dtype != dtype:
+#         raise TypeError("kmerdb.distance.correlation expects a KDBReader of '{0}' as its first argument".format(dtype))
+#     elif prof2.dtype != dtype:
+#         raise TypeError("kmerdb.distance.correlation expects a KDBReader of '{0}' as its second argument".format(dtype))
+#     elif k is None:
+#         raise ValueError("Cannot calculate correlation without k")
+#     p1_size = int(prof1.profile.size)
+#     p2_size = int(prof2.profile.size)
+#     if p1_size == p2_size:
+#         #print(p1_size)
+#         #print(p2_size)
+#         #print("Trying to calculate distance. Sizes are equal.")
+#         #sys.exit(1)
+#         pass
+#     if k is not None and p1_size != p2_size:
+#         logger.error(p1_size)
+#         logger.error(p2_size)
+#         sys.exit(1)
+#         raise ValueError("Cannot calculate correlation of unequal size NumPy arrays.")
+
+#     N = 4**k
+#     if p1_size == p2_size:
+#         x_bar = np.sum(prof1.profile)/prof1.metadata["total_kmers"]
+#         y_bar = np.sum(prof2.profile)/prof2.metadata["total_kmers"]
+
+#         ssxx = 0
+#         ssyy = 0
+#         ssxy = 0
+#         for kmer_id in range(N):
+#             x = prof1.profile[kmer_id]
+#             y = prof2.profile[kmer_id]
+#             ssxx += np.square(x - x_bar)
+#             ssyy += np.square(y - y_bar)
+#             x_minus_xbar = x - x_bar
+#             y_minus_ybar = y - y_bar
+#             ssxy += x_minus_xbar*y_minus_ybar
+#             logger.warning("Beware custom correlations")
+#         #x_bar = functools.reduce(lambda a,b: a+b, prof1))
+#         if ssxx*ssyy == 0:
+#             logger.error("Incorrect denominator found, skipping...")
+#             return 0
+#         else:
+#             logger.info("Acquired")
+#             logger.debug("Well done, cap...")
+#             return ssxy/np.sqrt(ssxx*ssyy)
+#     else:
+#         logger.error("Cannot manage to compare a {0}-dim profile with a {1}-dim profile...".format(p1_size, p2_size))
+#         raise ValueError("Cannot calculate the correlation coefficient of unequal arrays.")
 
     
-def _correlation(fname1, fname2):
-    if type(fname1) is not str:
-        raise TypeError("kmerdb.distance.correlation expects a str as its first positional argument")
-    elif type(fname2) is not str:
-        raise TypeError("kmerdb.distance.correlation expects a str as its second positional argument")
-    k = None
-    logger.info("Custom correlation coefficient calculation")
-    with fileutil.open(fname1, mode='r') as kdb1:
-        with fileutil.open(fname2, mode='r') as kdb2:
-            if k is None:
-                k = kdb1.metadata['k']
-            if k != kdb1.metadata['k']:
-                raise Exception("File '{0}' reported k = {1} instead of k = {2}".format(f, kdb1.metadata['k'], k))
-            elif k != kdb2.metadata['k']:
-                raise Exception("File '{0}' reported k = {1} instead of k = {2}".format(f, kdb2.metadata['k'], k))
-            N = 4 ** k
-            x_bar = functools.reduce(lambda a,b: a+b, map(lambda x: x['total_kmers'], kdb1.metadata['files']), 0) / N
-            y_bar = functools.reduce(lambda a,b: a+b, map(lambda y: y['total_kmers'], kdb2.metadata['files']), 0) / N
-            ## CALCULATE CORRELATION
-            ssxx = 0
-            ssyy = 0
-            ssxy = 0
-            for kmer_id in range(N):
-                line1 = next(kdb1)
-                line2 = next(kdb2)
-                _, x = (int(_x) for _x in line1.rstrip().split("\t")[0:2])
-                _, y = (int(_y) for _y in line2.rstrip().split("\t")[0:2])
-                ssxx += np.square(x - x_bar)
-                ssyy += np.square(y - y_bar)
-                ssxy += (x - x_bar)*(y - y_bar)
-                #logger.debug("int through np.type on custom correlation function")
-                #logger.debug("Style points")
-                logger.info("Squared differences.")
-                logger.info("Symmetry")
-            logger.debug("Sum of squared xy errors: {0}".format(ssxy))
-            logger.debug("Sum of squared xx errors: {0}".format(ssxx))
-            logger.debug("Sum of squared yy errors: {0}".format(ssyy))
-            if ssxx*ssyy == 0: # Handle an irrational number
-                return 0
-            else:
-                return ssxy/np.sqrt(ssxx*ssyy)
+# def _correlation(fname1, fname2):
+#     if type(fname1) is not str:
+#         raise TypeError("kmerdb.distance.correlation expects a str as its first positional argument")
+#     elif type(fname2) is not str:
+#         raise TypeError("kmerdb.distance.correlation expects a str as its second positional argument")
+#     k = None
+#     logger.info("Custom correlation coefficient calculation")
+#     with fileutil.open(fname1, mode='r') as kdb1:
+#         with fileutil.open(fname2, mode='r') as kdb2:
+#             if k is None:
+#                 k = kdb1.metadata['k']
+#             if k != kdb1.metadata['k']:
+#                 raise Exception("File '{0}' reported k = {1} instead of k = {2}".format(f, kdb1.metadata['k'], k))
+#             elif k != kdb2.metadata['k']:
+#                 raise Exception("File '{0}' reported k = {1} instead of k = {2}".format(f, kdb2.metadata['k'], k))
+#             N = 4 ** k
+#             x_bar = functools.reduce(lambda a,b: a+b, map(lambda x: x['total_kmers'], kdb1.metadata['files']), 0) / N
+#             y_bar = functools.reduce(lambda a,b: a+b, map(lambda y: y['total_kmers'], kdb2.metadata['files']), 0) / N
+#             ## CALCULATE CORRELATION
+#             ssxx = 0
+#             ssyy = 0
+#             ssxy = 0
+#             for kmer_id in range(N):
+#                 line1 = next(kdb1)
+#                 line2 = next(kdb2)
+#                 _, x = (int(_x) for _x in line1.rstrip().split("\t")[0:2])
+#                 _, y = (int(_y) for _y in line2.rstrip().split("\t")[0:2])
+#                 ssxx += np.square(x - x_bar)
+#                 ssyy += np.square(y - y_bar)
+#                 ssxy += (x - x_bar)*(y - y_bar)
+#                 #logger.debug("int through np.type on custom correlation function")
+#                 #logger.debug("Style points")
+#                 logger.info("Squared differences.")
+#                 logger.info("Symmetry")
+#             logger.debug("Sum of squared xy errors: {0}".format(ssxy))
+#             logger.debug("Sum of squared xx errors: {0}".format(ssxx))
+#             logger.debug("Sum of squared yy errors: {0}".format(ssyy))
+#             if ssxx*ssyy == 0: # Handle an irrational number
+#                 return 0
+#             else:
+#                 return ssxy/np.sqrt(ssxx*ssyy)
 
 
 
