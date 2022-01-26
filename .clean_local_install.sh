4bb621d1041b3d7b7cf32a38931dfda29279dd28
Matthew Ralston <mralston.development@gmail.com>
2022-01-26 01:43:57 -0500

    First commit towards a Cythonized Pearson correlation function.

--- .clean_local_install.sh
+++ .clean_local_install.sh
@@ -3,3 +3,8 @@
 
 
 rm -rf /home/matt/.pyenv/versions/kdb/lib/python3.10/site-packages/kmerdb /home/matt/.pyenv/versions/kdb/lib/python3.10/site-packages/kmerdb-*.egg-info /ffast2/kdb/kmerdb.egg-info /ffast2/kdb/build /ffast2/kdb/dist
+rm -rf /home/matt/.pyenv/versions/3.10.1/envs/kdb/lib/python3.10/site-packages/kmerdb-*
+cd /ffast2/kdb/
+rm -rf dist build kmerdb.egg-info
+cd
+
