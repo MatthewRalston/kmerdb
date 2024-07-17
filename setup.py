3ad2d5001e37518c89c575ecf035f9438d031044
Matthew Ralston <mralston.development@gmail.com>
2024-07-17 16:41:21 -0400

    Hotfix in init. Re-releasing as 0.8.5

--- setup.py
+++ setup.py
@@ -114,7 +114,7 @@ EMAIL = 'mralston.development@gmail.com'
 AUTHOR = 'Matt Ralston'
 #REQUIRES_PYTHON = ">=3.7.4"
 REQUIRES_PYTHON = '>=3.12.2'
-VERSION = "0.8.4"
+VERSION = "0.8.5"
 KEYWORDS = ["bioinformatics", "fastq", "fasta", "k-mer", "kmer"]
 CLASSIFIERS = [
 	    "Development Status :: 1 - Planning",
